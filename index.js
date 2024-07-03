c02788fcc41cb9803cf94d7211a6f494e3cd6326
itsalivenger <alihoussa16@gmail.com>
2024-07-03 23:32:50 +0100

    first commit

--- index.js
+++ index.js
@@ -6,31 +6,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Define allowed origins
-const allowedOrigins = ["https://itsalivenger.github.io/*", "http://127.0.0.1:5500", "https://itsalivenger.github.io/PentaBrood"];
+const allowedOrigins = ["https://itsalivenger.github.io", "http://127.0.0.1:5500"];
 
 // CORS configuration
 app.use(cors({
   origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}));
-
-// Handle preflight OPTIONS requests
-app.UseCors(x => x
-    .AllowAnyMethod()
-    .AllowAnyHeader()
-    .SetIsOriginAllowed(origin => true) // allow any origin
-    .AllowCredentials()); // allow credentials
-
-// Handle preflight OPTIONS requests
-app.options('*', cors({
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin)) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
@@ -45,20 +26,24 @@ app.use((req, res, next) => {
   next();
 });
 
-// Middleware to add CORS headers manually
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin); // Set specific origin
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
-
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 
 // Products Express Router
 app.use("/products", prodsRouter);
+
+// Handle preflight OPTIONS requests
+app.options('*', cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT} for requests`);
