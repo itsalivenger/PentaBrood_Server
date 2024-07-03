725451202f73d24b071938bc9db48272a060e8ca
itsalivenger <alihoussa16@gmail.com>
2024-07-03 22:29:49 +0100

    first commit

--- index.js
+++ index.js
@@ -23,6 +23,13 @@ app.use((req, res, next) => {
   next();
 });
 
+// Add explicit CORS headers to all responses
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
