667ea994194c8e37b7141abe322277361d33eeae
itsalivenger <alihoussa16@gmail.com>
2024-07-16 18:39:22 +0100

    indexRoute

---
+++ routes/indexRoute.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    const { type } = req.body;
+
+    if(type == 'subscribe'){
+        const email = req.body.email;
+        console.log(email);
+    }
+    res.send({testps: `wslat l3iba f j3iba ${req.body}`});
+});
